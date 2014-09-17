a45077c4979a86dc59f70e0d59c546ee43cc9beb
JoAnn Brooks <joannbrooks036@gmail.com>
2014-09-17 11:28:47 -0500

    initial files

---
+++ client/views/home/home.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+
+  angular.module('directives-demo')
+  .controller('HomeCtrl', ['$scope', '$interval', 'Home', function($scope, $interval, Home){
+  }]);
+})();
+
---
+++ client/views/logout/logout.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular.module('directives-demo')
+  .controller('LogoutCtrl', ['$location', 'User', function($location, User){
+    User.logout().then(function(){
+      toastr.success('Successful logout.');
+      $location.path('/');
+    });
+  }]);
+})();
+
